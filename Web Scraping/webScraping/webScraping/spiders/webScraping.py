3cfaf0a363c40f01a8aa99e023ed08dccccf40b3
AdeenaLathiya <adeenalathiya@gmail.com>
2022-04-15 18:21:29 +0500

    dawn and tribune scraped

--- Web Scraping/webScraping/webScraping/spiders/webScraping.py
+++ Web Scraping/webScraping/webScraping/spiders/webScraping.py
@@ -1,4 +1,5 @@
 import scrapy
+import json
 from scrapy import Request
 from ..items import WebscrapingItem
 
@@ -11,94 +12,48 @@ class WebscrapingSpider(scrapy.Spider):
       yield Request('https://www.dawn.com/sport',callback=self.DawnSports)
       yield Request('https://www.dawn.com/tech',callback=self.DawnTech)
 
-      #calling Tribune Categories
+      # #calling Tribune Categories
       yield Request('https://tribune.com.pk/Business',callback=self.TribuneBusiness)
       yield Request('https://tribune.com.pk/Sports',callback=self.TribuneSports)
       yield Request('https://tribune.com.pk/Technology',callback=self.TribuneTech)
 
   def DawnBusiness(self,response):
     for newsLink in response.css("h2.story__title a::attr(href)"):
-      yield response.follow(newsLink.get(), callback=self.parseDawnBusiness)
+      yield response.follow(newsLink.get(), callback=self.parseDawn, meta={'category': 'business','source': 'DAWN'})
 
   def DawnSports(self,response):
       for newsLink in response.css("h2.story__title a::attr(href)"):
-        yield response.follow(newsLink.get(), callback=self.parseDawnSports)
+        yield response.follow(newsLink.get(), callback=self.parseDawn, meta={'category': 'sports','source': 'DAWN'})
 
   def DawnTech(self,response):
       for newsLink in response.css("h2.story__title a::attr(href)"):
-        yield response.follow(newsLink.get(), callback=self.parseDawnTech)
+        yield response.follow(newsLink.get(), callback=self.parseDawn, meta={'category': 'tech','source': 'DAWN'})
 
   def TribuneBusiness(self,response):
       for all_news_link in response.css('ul.listing-page li div.row div.col-md-8 div.horiz-news3-caption a::attr(href)'):
-        yield response.follow(all_news_link.get(), callback=self.parseTribuneBusiness)
+        yield response.follow(all_news_link.get(), callback=self.parseTribuneBusiness, meta={'category': 'business','source': 'Tribune' })
 
   def TribuneSports(self,response):
       for all_news_link in response.css('ul.listing-page li div.row div.col-md-8 div.horiz-news3-caption a::attr(href)'):
-        yield response.follow(all_news_link.get(), callback=self.parseTribuneSports)
+        yield response.follow(all_news_link.get(), callback=self.parseTribune, meta={'category': 'sports','source': 'Tribune'})
 
   def TribuneTech(self,response):
       for all_news_link in response.css('ul.listing-page li div.row div.col-md-8 div.horiz-news3-caption a::attr(href)'):
-        yield response.follow(all_news_link.get(), callback=self.parseTribuneTech)            
+        yield response.follow(all_news_link.get(), callback=self.parseTribune, meta={'category': 'tech','source': 'Tribune'})            
 
 
 
-  def parseDawnBusiness(self, response):
+  def parseDawn(self, response):
     items = WebscrapingItem()
 
     title = response.css("h2.story__title a.story__link::text").extract_first().strip() ,
     author = response.css("span.story__byline a.story__byline__link::text").extract_first() ,
     content = response.css("p::text").extract(),
-    image = response.css("figure.media--uneven div.media__item  picture img::attr(src)").extract(),
+    image = response.css("figure.media--uneven div.media__item  picture img::attr(src)")[0].extract(),
     time = response.css("span.story__time span.timestamp--time span.timestamp__time::text").extract(), 
     date = response.css("span.story__time span.timestamp--date::text").extract()  
-    category = 'business'
-    source = 'DAWN'
-
-    items['title'] = title
-    items['author'] = author
-    items['content'] = content
-    items['image'] = image
-    items['time'] = time
-    items['date'] = date
-    items['category'] = category
-    items['source'] = source
-
-    yield items
-
-  def parseDawnSports(self, response):
-    items = WebscrapingItem()
-
-    title = response.css("h2.story__title a.story__link::text").extract_first().strip() ,
-    author = response.css("span.story__byline a.story__byline__link::text").extract_first() ,
-    content = response.css("p::text").extract(),
-    image = response.css("figure.media--uneven div.media__item  picture img::attr(src)").extract(),
-    time = response.css("span.story__time span.timestamp--time span.timestamp__time::text").extract(), 
-    date = response.css("span.story__time span.timestamp--date::text").extract() 
-    category = 'sports'
-    source = 'DAWN'
-
-    items['title'] = title
-    items['author'] = author
-    items['content'] = content
-    items['image'] = image
-    items['time'] = time
-    items['date'] = date
-    items['category'] = category
-    items['source'] = source
-
-    yield items
-
-  def parseDawnTech(self, response):
-    items = WebscrapingItem()
-
-    title = response.css("h2.story__title a.story__link::text").extract_first().strip() ,
-    author = response.css("span.story__byline a.story__byline__link::text").extract_first() ,
-    content = response.css("p::text").extract(),
-    image = response.css("figure.media--uneven div.media__item  picture img::attr(src)").extract(),
-    time = response.css("span.story__time span.timestamp--time span.timestamp__time::text").extract(), 
-    date = response.css("span.story__time span.timestamp--date::text").extract() 
-    category = 'tech'
-    source = 'DAWN'
+    category = response.meta['category']
+    source = response.meta['source']
 
     items['title'] = title
     items['author'] = author
@@ -116,66 +71,46 @@ class WebscrapingSpider(scrapy.Spider):
 
     title = response.css('div.story-box-section h1::text').get()
     author = response.css('div.story-box-section span.storypage-leftside div.left-authorbox span a::text').get()
-    content = response.css('div.story-box-section div.mainstorycontent-parent div.storypage-main-section2 div.storypage-rightside span.story-text p span::text').get().strip()
+    # content = response.meta['content']
+    content = response.css('p > span:nth-child(1)::text , p:nth-child(3)::text , .location-names::text').extract()
     image = response.css('div.story-box-section div.mainstorycontent-parent div.storypage-main-section2 div.storypage-rightside span.top-big-img div.story-featuredimage div.amp-top-main-img div.featured-image-global img::attr(src)').get()
     date = response.css('div.story-box-section span.storypage-leftside div.left-authorbox span::text')[1].get()
     time = response.xpath('//script[@type="application/ld+json"]/text()').get()
-    category = 'business'
-    source = 'Express Tribune'
+    json_data = json.loads(time, strict=False)
+    category = response.meta['category']
+    source = response.meta['source']
 
     items['title'] = title
     items['author'] = author
     items['content'] = content
     items['image'] = image
-    items['time'] = time
+    items['time'] = json_data['datePublished']
     items['date'] = date
     items['category'] = category
     items['source'] = source
 
     yield items
 
-  def parseTribuneSports(self, response):
+  def parseTribune(self, response):
     items = WebscrapingItem()
 
     title = response.css('div.story-box-section h1::text').get()
     author = response.css('div.story-box-section span.storypage-leftside div.left-authorbox span a::text').get()
-    content = response.css('div.story-box-section div.mainstorycontent-parent div.storypage-main-section2 div.storypage-rightside span.story-text p::text').get().strip()
+    content = response.css('.story-text p::text').extract()
     image = response.css('div.story-box-section div.mainstorycontent-parent div.storypage-main-section2 div.storypage-rightside span.top-big-img div.story-featuredimage div.amp-top-main-img div.featured-image-global img::attr(src)').get()
     date = response.css('div.story-box-section span.storypage-leftside div.left-authorbox span::text')[1].get()
     time = response.xpath('//script[@type="application/ld+json"]/text()').get()
-    category = 'sports'
-    source = 'Express Tribune'
+    json_data = json.loads(time, strict=False)
+    category = response.meta['category']
+    source = response.meta['source']
 
     items['title'] = title
     items['author'] = author
     items['content'] = content
     items['image'] = image
-    items['time'] = time
+    items['time'] = json_data['datePublished']
     items['date'] = date
     items['category'] = category
     items['source'] = source
 
     yield items  
-
-  def parseTribuneTech(self, response):
-    items = WebscrapingItem()
-
-    title = response.css('div.story-box-section h1::text').get()
-    author = response.css('div.story-box-section span.storypage-leftside div.left-authorbox span a::text').get()
-    content = response.css('div.story-box-section div.mainstorycontent-parent div.storypage-main-section2 div.storypage-rightside span.story-text p::text').get().strip()
-    image = response.css('div.story-box-section div.mainstorycontent-parent div.storypage-main-section2 div.storypage-rightside span.top-big-img div.story-featuredimage div.amp-top-main-img div.featured-image-global img::attr(src)').get()
-    date = response.css('div.story-box-section span.storypage-leftside div.left-authorbox span::text')[1].get()
-    time = response.xpath('//script[@type="application/ld+json"]/text()').get()
-    category = 'sports'
-    source = 'Express Tribune'
-
-    items['title'] = title
-    items['author'] = author
-    items['content'] = content
-    items['image'] = image
-    items['time'] = time
-    items['date'] = date
-    items['category'] = category
-    items['source'] = source
-
-    yield items    
