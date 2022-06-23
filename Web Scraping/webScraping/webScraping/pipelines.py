2bc76998fc70af3b3d0ceb1f3a54bb6806a63fdc
AdeenaLathiya <adeenalathiya@gmail.com>
2022-06-23 13:43:04 +0500

    cleaned the scraped content

--- Web Scraping/webScraping/webScraping/pipelines.py
+++ Web Scraping/webScraping/webScraping/pipelines.py
@@ -25,12 +25,24 @@ class WebscrapingPipeline(object):
 
     def process_item(self, item, spider):
         news = ItemAdapter(item).asdict()
-        summary = self.text_summarizer(news['content'])
+
+        content = news['content']
+
+        extraString = 'Compunode.com Pvt. Ltd. ( ).Designed for  . Copyright © 2022, Dawn Scribe Publishing Platform' 
+        extraString2 = "Updated"
+
+        if extraString in content:
+            content = content.replace(extraString, '')
+        if extraString2 in content:
+            content = content.split(extraString2, 1)
+            content = content[0]
+
+        summary = self.text_summarizer(content)
 
         news = {
             "title": news['title'],
             "author": news['author'],
-            "content": news['content'],
+            "content": content,
             "image": news['image'],
             "time": news['time'],
             "date": news['date'],
